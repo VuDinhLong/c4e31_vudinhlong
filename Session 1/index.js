6ef55e3b96402d88371d45d6628dc81673a4d703
VuDinhLong <TommyVu@VuDinhLongs-MacBook-Air.local>
2019-04-21 23:16:08 +0700

    session1

---
+++ Session 1/index.js
@@ -0,0 +1,32 @@
+// // let name = prompt("Enter Your Name");
+// // console.log("Hello", name)
+
+// const x= prompt("PLease Enter X");
+// let y=prompt("Please Enter Y");
+// console.log(x,"+",y,"=",Number(x) + Number(y));
+
+// // 1.Tạo file html
+// // 2.Gõ html, chọn html:5 để hiển thị khung html
+// // 3.Tạo file js để code js
+// // 4.Nối 2 file với nhau bằng thông qua script
+// // 5.Chạy file html
+// // 6.Inspect trình duyệt
+// // 7.Chuyển qua tab console để xem kết quả
+
+// for(let i=1;i<=2;i++)
+// {   fd(100)
+//     rt(90)
+//     fd(100)
+//     rt(90)
+//     fd(100)
+//     rt(90)
+//     fd(100)
+    
+//     rt(120)}
+
+// // loops
+
+for(let i=0;i<5;i++){
+    console.log(i)
+    console.log("")
+}
