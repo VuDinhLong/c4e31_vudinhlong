ec697f3ba36c52961b13f47089d879ead0508ff1
VuDinhLong <TommyVu@VuDinhLongs-MacBook-Air.local>
2019-04-27 18:16:00 +0700

    session2

---
+++ Session 2/index.js
@@ -0,0 +1,101 @@
+// for(u=1; u<=100; u++){
+//     console.log(u)
+// }
+// for(i=100;i>=1;i--){
+//     console.log(i)
+// }
+// i+=
+// // i=i+2
+// // let len=5
+// // for(i=1;i<=100;i+=1)
+// // { fd(len)
+// // rt(21)
+// // len+=10}
+// // clear()
+
+// let tổng=0
+// let n=prompt("Nhập số n")
+// for(i=1;i<=n;i++){
+//  tổng+=i}
+// console.log(tổng)
+
+// CONDITIONAL / BRANCH 
+// let age=prompt("Enter your age");
+// if(age<10)
+//     {console.log("Baby")} 
+// else if(age<18)
+//     {console.log("Teenager")}
+// else if(age<25)
+// //     {console.log("Trẻ trâu")}
+// // else
+// //     {console.log("Adult")} 
+
+// for(i=1;i<=100;i++)
+//     {if(i%2==0)
+//     console.log("i")}
+
+// === so sánh tuyệt đối(so cả kiểu dữ liệu)
+// == so sánh 2 vế
+// = gán giá trị vào biến
+
+// max=100
+// min=1
+// n=Math.floor(Math.random()*100+1)
+// if(n<=30)
+//     {console.log("Sunny")}
+// else if (n<=70) 
+//     {console.log("Rainy")}
+// else
+//     {console.log("Cloudy")}
+// console.log(n)
+// // Math.floor : làm tròn
+
+// TÌM NGHIỆM PHƯƠNG TRÌNH BẬC HAI
+// a=prompt("Nhập a")
+// b=prompt("Nhập b")
+// c=prompt("Nhập c")
+// delta=b**2-4*a*c
+// if(delta>0)
+//     {console.log("2 Nghiệm phân biệt")
+//     console.log((-Number(b)-Math.sqrt(delta))/(2*a))
+//     console.log((-Number(b)+Math.sqrt(delta))/(2*a))}
+// else if(delta===0)
+//     {console.log("1 Nghiệm kép")
+//     console.log((-Number(b))/(2*a))}
+// else    
+//     {console.log("Vô nghiệm")}
+
+// VẼ MŨI TÊN XANH ĐỎ
+// color("red")
+// rt(90)
+// for(i=0;i<15;i++){
+//   if(i%2==0){
+//     color("red")}
+ //   else{
+//       color("blue")}
+//       penup()
+//       fd(10)
+//       pendown()
+//       fd(20)}
+// clear()
+
+
+// // // KIỂM TRA USERNAME VÀ PASSWORD 
+// username=prompt("Nhập username")
+// if(username=="C4E31"){
+//     password=prompt("Nhập password")
+//     if(password=="mindx"){
+//         console.log("Welcome")}
+//     else{
+//         console.log("Wrong Password")
+//     }
+// }
+// else{
+//     console.log("Wrong Username")
+// }
+
+// BOOLEAN
+// a=1
+// b=9
+// console.log(Boolean(a > b)) 
+console.log(5==6)
